2964cd7a19675ca3c6a0d27d9c0133d87a582110
philemon <philemonmaosa@gmail.com>
2025-10-08 21:40:03 +0300

    Added url paths

--- homemenu_app/urls.py
+++ homemenu_app/urls.py
@@ -26,6 +26,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('', home, name='home'),
     path('admin/', admin.site.urls),
+    path('api/users/', include('users.urls')),
     path('api/auth/register/', RegisterView.as_view(), name='register'),
     path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
