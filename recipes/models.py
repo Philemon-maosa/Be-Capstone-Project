6b2995f1f002471288cb8d543387300c4b5feef4
philemon <philemonmaosa@gmail.com>
2025-10-08 21:09:27 +0300

    Added ingredients and recipes models

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,3 @@
-from django.db import models
-
 # Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
@@ -15,9 +13,10 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    quantity= models.CharField(max_length=50, blank=True, null=True)
     instructions = models.TextField()
     ingredients = models.ManyToManyField(Ingredient, related_name="recipes")
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="ingredients")
 
     def __str__(self):
-        return self.name
+        return f"{self.name} ({self.quantity or ''} {self.unit or ''})"
--- recipes/serializers.py
+++ recipes/serializers.py
@@ -4,12 +4,21 @@ from .models import Ingredient, Recipe
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
-        fields = ['id', 'name', 'owner']
+        fields = ['id', 'name', 'quantity', 'unit']
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-    ingredients = IngredientSerializer(many=True, read_only=True)
+    ingredients = IngredientSerializer(many=True)
 
     class Meta:
         model = Recipe
-        fields = ['id', 'name', 'instructions', 'ingredients', 'owner']
+        fields = ['id', 'name', 'description', 'instructions', 'ingredients', 'owner']
+        read_only_fields = ['owner']
+
+    def create(self, validated_data):
+        ingredients_data = validated_data.pop('ingredients')
+        recipe = Recipe.objects.create(**validated_data)
+        for ing in ingredients_data:
+            ingredient, _ = Ingredient.objects.get_or_create(**ing)
+            recipe.ingredients.add(ingredient)
+        return recipe
